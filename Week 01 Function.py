b86b3e6890e2c77e43cef9ec368d0f67560c65a0
AamirAli <Aamirk4560@gmail.com>
2024-01-26 11:12:11 +0500

    Added Function Working

--- Week 01 Function.py
+++ Week 01 Function.py
@@ -14,7 +14,7 @@ def describe_pet(animal_type, pet_name):
 describe_pet(animal_type='cat', pet_name='Mano')
 
 #Positional Arguments
-def describe_pet(animal_type, pet_name:
+def describe_pet(animal_type, pet_name):
     """Display information about a pet."""
     print(f"\nI have a: {animal_type.title()}")
     print(f"My {animal_type}'s name is {pet_name.title()}")
